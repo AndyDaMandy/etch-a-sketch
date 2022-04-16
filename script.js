dfcedeb06bd12792caa4e1131272837bbc26b5df
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-04-16 12:05:06 -0400

    added embedded function

--- script.js
+++ script.js
@@ -6,7 +6,16 @@ function addBoxes (x) {
     } else if (x < 12){
         alert("Going below 12 is too low");
     }
+    //two for loops
+    //first one creates a row container
+    //second one inside creates each line in the row
     for (let i = 0; i < x; i++){
-      let b = document.createElement("div");
+      let row = document.createElement("div");
+      row.className = 'row';
+      for (let b = 0; b < x; b++){
+          let rowData = document.createElement("div");
+          row.appendChild(rowData);
+      }
+      container.appendChild(row);
     }
 }
